4154867b5fdc07fd45261e3e8c2e3b356ce91003
Jonas Oliveira <jonascsoliveira@outlook.com>
2020-04-20 23:20:26 -0300

    Criando Repository e service Categoria

---
+++ src/main/java/com/jonasoliveira/lojaweb/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.jonasoliveira.lojaweb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jonasoliveira.lojaweb.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
