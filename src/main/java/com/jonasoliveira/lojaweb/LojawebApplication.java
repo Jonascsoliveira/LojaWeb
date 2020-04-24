e3b3891defcb886ff961d0608b27b59151d7a9fd
Jonas Oliveira <jonascsoliveira@outlook.com>
2020-04-24 18:20:57 -0300

    Classes Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/jonasoliveira/lojaweb/LojawebApplication.java
+++ src/main/java/com/jonasoliveira/lojaweb/LojawebApplication.java
@@ -1,5 +1,6 @@
 package com.jonasoliveira.lojaweb;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.jonasoliveira.lojaweb.domain.Cidade;
 import com.jonasoliveira.lojaweb.domain.Cliente;
 import com.jonasoliveira.lojaweb.domain.Endereco;
 import com.jonasoliveira.lojaweb.domain.Estado;
+import com.jonasoliveira.lojaweb.domain.Pagamento;
+import com.jonasoliveira.lojaweb.domain.PagamentoComBoleto;
+import com.jonasoliveira.lojaweb.domain.PagamentoComCartao;
+import com.jonasoliveira.lojaweb.domain.Pedido;
 import com.jonasoliveira.lojaweb.domain.Produto;
+import com.jonasoliveira.lojaweb.domain.enums.EstadoPagamento;
 import com.jonasoliveira.lojaweb.domain.enums.TipoCliente;
 import com.jonasoliveira.lojaweb.repositories.CategoriaRepository;
 import com.jonasoliveira.lojaweb.repositories.CidadeRepository;
 import com.jonasoliveira.lojaweb.repositories.ClienteRepository;
 import com.jonasoliveira.lojaweb.repositories.EnderecoRepository;
 import com.jonasoliveira.lojaweb.repositories.EstadoRepository;
+import com.jonasoliveira.lojaweb.repositories.PagamentoRepository;
+import com.jonasoliveira.lojaweb.repositories.PedidoRepository;
 import com.jonasoliveira.lojaweb.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -35,7 +43,11 @@ public class LojawebApplication implements CommandLineRunner{
 	@Autowired
 	private ClienteRepository clienteRepository;
 	@Autowired
-	private EnderecoRepository enderecoReposiory;	
+	private EnderecoRepository enderecoReposiory;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LojawebApplication.class, args);
@@ -86,6 +98,24 @@ public class LojawebApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoReposiory.saveAll(Arrays.asList(e1,e2));
+		
+		/* Instanciando Pedidos*/
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("24/04/2020 17:44"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("20/04/2019 20:44"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null,EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2019 00:20"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
