4384f666e3dc01b8c6dbeb98ed9aba13b15d8d81
Jonas Oliveira <jonascsoliveira@outlook.com>
2020-08-08 22:31:26 -0300

    validacao sintatica com Bean Validation

--- src/main/java/com/jonasoliveira/lojaweb/services/CategoriaService.java
+++ src/main/java/com/jonasoliveira/lojaweb/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jonasoliveira.lojaweb.domain.Categoria;
+import com.jonasoliveira.lojaweb.dto.CategoriaDTO;
 import com.jonasoliveira.lojaweb.repositories.CategoriaRepository;
 import com.jonasoliveira.lojaweb.services.exceptions.DataIntegrityException;
 import com.jonasoliveira.lojaweb.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
