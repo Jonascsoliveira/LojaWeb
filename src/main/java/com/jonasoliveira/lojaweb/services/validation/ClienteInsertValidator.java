f86ff12fae5c417aaba9051cc764bc10f314b05d
Jonas Oliveira <52612513+Jonascsoliveira@users.noreply.github.com>
2021-01-17 14:45:58 -0300

    Adicioanndo tratamento para evitar insercao de email repetido

--- src/main/java/com/jonasoliveira/lojaweb/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/jonasoliveira/lojaweb/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jonasoliveira.lojaweb.domain.Cliente;
 import com.jonasoliveira.lojaweb.domain.enums.TipoCliente;
 import com.jonasoliveira.lojaweb.dto.ClienteNewDTO;
+import com.jonasoliveira.lojaweb.repositories.ClienteRepository;
 import com.jonasoliveira.lojaweb.resources.exception.FieldMessage;
 import com.jonasoliveira.lojaweb.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		// inclua os testes aqui, inserindo erros na lista
