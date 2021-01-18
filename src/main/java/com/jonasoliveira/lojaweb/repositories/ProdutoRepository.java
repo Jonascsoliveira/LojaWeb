54cfb01d68932c07ad5cef0f57d4a6b4b30685d9
Jonas Oliveira <52612513+Jonascsoliveira@users.noreply.github.com>
2021-01-17 23:29:26 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/jonasoliveira/lojaweb/repositories/ProdutoRepository.java
+++ src/main/java/com/jonasoliveira/lojaweb/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.jonasoliveira.lojaweb.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jonasoliveira.lojaweb.domain.Categoria;
 import com.jonasoliveira.lojaweb.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
+	/*Quando houver query, a mesma sobrepõe o nome de método, Anotação Transactional é mais performática comreadOnly true*/
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
---
+++ src/main/java/com/jonasoliveira/lojaweb/services/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.jonasoliveira.lojaweb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.jonasoliveira.lojaweb.domain.Categoria;
+import com.jonasoliveira.lojaweb.domain.Produto;
+import com.jonasoliveira.lojaweb.repositories.CategoriaRepository;
+import com.jonasoliveira.lojaweb.repositories.ProdutoRepository;
+import com.jonasoliveira.lojaweb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+		}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+}
