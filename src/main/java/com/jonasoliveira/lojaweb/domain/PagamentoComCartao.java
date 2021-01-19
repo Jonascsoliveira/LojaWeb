391d2b14e05f78732d0ee6279f038963d5b86739
Jonas Oliveira <52612513+Jonascsoliveira@users.noreply.github.com>
2021-01-18 22:05:20 -0300

    Inserindo pedido

--- src/main/java/com/jonasoliveira/lojaweb/domain/PagamentoComCartao.java
+++ src/main/java/com/jonasoliveira/lojaweb/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.jonasoliveira.lojaweb.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.jonasoliveira.lojaweb.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
