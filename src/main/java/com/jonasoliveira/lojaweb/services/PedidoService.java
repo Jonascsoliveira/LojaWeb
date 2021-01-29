c0f0af394796f3e2a4b2188966316dd72c5924e1
Jonas Oliveira <52612513+Jonascsoliveira@users.noreply.github.com>
2021-01-28 21:22:19 -0300

    restricao de conteudo, cliente recupera somente seu pedidos

--- src/main/java/com/jonasoliveira/lojaweb/services/PedidoService.java
+++ src/main/java/com/jonasoliveira/lojaweb/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jonasoliveira.lojaweb.domain.Cliente;
 import com.jonasoliveira.lojaweb.domain.ItemPedido;
 import com.jonasoliveira.lojaweb.domain.PagamentoComBoleto;
 import com.jonasoliveira.lojaweb.domain.Pedido;
@@ -14,6 +18,8 @@ import com.jonasoliveira.lojaweb.domain.enums.EstadoPagamento;
 import com.jonasoliveira.lojaweb.repositories.ItemPedidoRepository;
 import com.jonasoliveira.lojaweb.repositories.PagamentoRepository;
 import com.jonasoliveira.lojaweb.repositories.PedidoRepository;
+import com.jonasoliveira.lojaweb.security.UserSS;
+import com.jonasoliveira.lojaweb.services.exceptions.AuthorizationException;
 import com.jonasoliveira.lojaweb.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -68,5 +74,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
