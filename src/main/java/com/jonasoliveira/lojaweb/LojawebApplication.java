6d0443d390ae894313fb0e02072eebe10ac62d11
Jonas Oliveira <52612513+Jonascsoliveira@users.noreply.github.com>
2021-02-01 23:27:01 -0300

    Salvando arquivo no S3

--- src/main/java/com/jonasoliveira/lojaweb/LojawebApplication.java
+++ src/main/java/com/jonasoliveira/lojaweb/LojawebApplication.java
@@ -1,11 +1,17 @@
 package com.jonasoliveira.lojaweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jonasoliveira.lojaweb.services.S3Service;
+
 @SpringBootApplication
 public class LojawebApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LojawebApplication.class, args);
@@ -13,7 +19,7 @@ public class LojawebApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\Users\\Jonas\\OneDrive\\Imagens\\tmuxexemplo.png");
 	}
 
 }
